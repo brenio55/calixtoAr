252da7bfbb749fa31e7a661a838649298780aaa4
Brenio Hallison Filho <brenio55@outlook.com>
2025-01-15 15:23:22 -0300

    fix: fixed airConditioner not appearing anymore

--- src/components/MainBanner/MainBanner.jsx
+++ src/components/MainBanner/MainBanner.jsx
@@ -4,7 +4,7 @@ const MainBanner = () => {
   return (
     <div className="w-layout-blockcontainer container-2 w-container">
       <div className="w-layout-blockcontainer container-3 w-container">
-        <img src="/img/arconditioner.svg" loading="lazy" alt="Ar Condicionado" className="heroAirConditioner" />
+        <img src="/img/airconditioner.svg" loading="lazy" alt="Ar Condicionado" className="heroAirConditioner" />
         <h1 className="heading">Mais de <span className="text-span">2 Mi</span> de BTUs Sob Nossa Responsabilidade</h1>
         <div className="subTitleHeroBanner">Garantindo o conforto e Economia para Sua Empresa, <span className="text-span-2">desde 1968</span></div>
         <a href="#quem-somos" className="button w-button buttonMainBanner">
